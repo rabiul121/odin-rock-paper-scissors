dcdff52bdcd9732f26dcf041cf5d0b1f6ad8642b
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2022-12-01 23:23:28 +0600

    finished the game and checked everything

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
 }
 
 // get playerChoice
-function playerChoice() {
+function getPlayerChoice() {
     let isValidInput = false;
     while (isValidInput == false) {
         const choice = prompt("Enter Rock Paper or Scissors");
@@ -21,8 +21,6 @@ function playerChoice() {
             return choiceInLowerCase;
         }
     }
-
-
 }
 
 // find the winner for a single round
@@ -42,23 +40,45 @@ function findWinner(playerSelection, computerSelection) {
 // write a function that plays a single round of game
 function playRound(playerSelection, computerSelection) {
     const roundResults = findWinner(playerSelection, computerSelection);
-    // console.log(roundResults);
     if (roundResults == "Tie") {
-        return "This is a tie!";
+        return "This is a Tie!";
     } else if (roundResults == "Player") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`;
+    } else if (roundResults == "Computer") {
+        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`;
     }
-
 }
+
 // make a function to play the game five rounds
 function game() {
-    console.log("Welcome to the game!");
+    console.log("|\t--------------------\t|");
+    console.log("|\tWelcome to the game!\t|");
+    console.log("|\t--------------------\t|");
+    console.log("\n");
+
+    let playerScore = 0;
+    let computerScore = 0;
+
     for (let i = 0; i < 5; i++) {
-        const playerSelection = playerChoice();
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
+        console.log(`Round: ${i + 1}`);
         console.log(playRound(playerSelection, computerSelection));
+        console.log("\n");
+        if (findWinner(playerSelection, computerSelection) == "Player") {
+            playerScore++;
+        } else if (findWinner(playerSelection, computerSelection) == "Computer") {
+            computerScore++;
+        }
+    }
+
+    console.log(`\nYour Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}\n`);
+
+    if (playerScore > computerScore) {
+        console.log("You Won the game!");
+    } else if (playerScore < computerScore) {
+        console.log("Computer Won the game!");
     }
 }
 
